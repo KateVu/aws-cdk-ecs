b32859adb750d305017f39da558f706ef27206c7
KateVu <tuquyen87@gmail.com>
2024-07-22 02:20:28 +1000

    using variables in github

--- bin/index.ts
+++ bin/index.ts
@@ -12,9 +12,7 @@ const accountFileName = 'aws_account.yaml'
 const envName = process.env.ENVIRONMENT_NAME || 'kate'
 const accountName = process.env.ACCOUNT_NAME || 'sandpit1'
 const region = process.env.REGION || 'ap-southeast-2'
-
-//Get aws account id
-const accountId = getAccountId(accountName, configFolder, accountFileName)
+const accountId = process.env.AWS_ACCOUNT_ID || 'none'
 
 const app = new cdk.App();
 
